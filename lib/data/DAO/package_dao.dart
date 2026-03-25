d73d6d60b8d78914dee8d0d0c34ef140494ba8d7
Dave <davybitencourt26@gmail.com>
2026-03-25 15:51:22 -0400

    refactor: package

--- lib/data/DAO/package_dao.dart
+++ lib/data/DAO/package_dao.dart
@@ -1,27 +1,45 @@
 import '../../data/database_helper.dart';
 
 class PackageDao {
+
   Future<List<Map<String, dynamic>>> getAll() async {
     final db = await DatabaseHelper.instance.database;
-    return db.query('package');
+    return db.rawQuery(
+      '''
+        SELECT id, session_id, title
+        FROM package
+      '''
+    );
   }
 
-  Future<void> insert(Map<String, dynamic> data) async {
+  Future<void> insert(int? sessionId, String title) async {
     final db = await DatabaseHelper.instance.database;
+    await db.rawInsert(
+      '''
+        INSERT INTO package (session_id, title)
+        VALUES (?, ?)
+      ''', [sessionId, title]
+    );
+  }
 
-    await db.insert(
-      'package', 
-      data
+  Future<void> update(int id, int? sessionId, String title) async {
+    final db = await DatabaseHelper.instance.database;
+    await db.rawUpdate(
+      '''
+        UPDATE package
+        SET session_id = ?, title = ?
+        WHERE id = ?
+      ''', [sessionId, title, id]
     );
   }
 
   Future<void> delete(int id) async {
     final db = await DatabaseHelper.instance.database;
-
-    await db.delete(
-      'package', 
-      where: 'id = ?', 
-      whereArgs: [id]
+    await db.rawDelete(
+      '''
+        DELETE FROM package
+        WHERE id = ?
+      ''', [id]
     );
   }
 }
