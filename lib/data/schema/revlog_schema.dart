3c836009e1ed92962cc671735d0ad1bae1da74de
Dave <davybitencourt26@gmail.com>
2026-04-03 18:49:06 -0400

    refactor: revlog

--- lib/data/schema/revlog_schema.dart
+++ lib/data/schema/revlog_schema.dart
@@ -1,5 +1,18 @@
-import 'package:sysyphus_learning_app/data/DAO/revlog_dao.dart';
+import '../DAO/revlog_dao.dart';
 
 class RevlogSchema {
-  
+  final RevlogDao _dao = RevlogDao();
+
+  Future<Map<DateTime, int>> getHeatmapData() async {
+    final rawData = await _dao.getCountPerDay();
+
+    final Map<DateTime, int> heatmapData = {};
+
+    rawData.forEach((dateString, count) {
+      final date = DateTime.parse(dateString);
+      heatmapData[date] = count;
+    });
+
+    return heatmapData;
+  }
 }
